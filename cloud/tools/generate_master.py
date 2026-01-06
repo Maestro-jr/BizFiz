45e37282744a8fd425a2a07af9fe89d99a99ced8
Maestro-jr <146443394+Maestro-jr@users.noreply.github.com>
2026-01-06 15:25:23 +0100

    delete

--- cloud/tools/generate_master.py
+++
@@ -1,8 +0,0 @@
-from  cloud.shared.security.passwords import create_master_credentials
-
-key_path = "cloudTemplateProject/credentials/master.key"
-enc_path = "cloudTemplateProject/credentials/master.enc"
-
-password = create_master_credentials(key_path, enc_path)
-print("MASTER PASSWORD:", password)
-print("Stored encrypted in credentials folder.")
